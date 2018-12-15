8be35ff886a46b44de985d3200926cc3b7ac95bd
Armando Neto <jose_netopio_12@hotmail.com>
2018-12-15 15:03:27 -0300

    Seção 3 Aula 27
    Componentes Filhos

---
+++ ex.5/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SilvaFamilly from './silvaFamilly'
+
+ReactDOM.render(
+
+    <SilvaFamilly/>
+
+, document.getElementById('app'))
+
+
--- ex/index.jsx
+++ ex/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SilvaFamilly from './silvaFamilly'
+import Family from './family'
+import Member from './member'
 
 ReactDOM.render(
 
-    <SilvaFamilly/>
+    <Family>
+        <Member name='Guilherme' lastName='Silva'/>
+    </Family>
 
 , document.getElementById('app'))
 
