efaa2c6f244906d37e808f1842d952bc94774105
Armando Neto <jose_netopio_12@hotmail.com>
2018-12-16 12:21:47 -0300

    Seção 3 Aula 30
    Componente de Classe

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Family from './family'
-import Member from './member'
+import ClassComponent from './classComponent'
 
 ReactDOM.render(
-
-    <Family lastName='Silva'>
-        <Member name='Guilherme'/>
-        <Member name='Rafael'/>
-        <Member name='Julia'/>
-    </Family>
-
+    <ClassComponent value='componente Classe' />
 , document.getElementById('app'))
 
 
